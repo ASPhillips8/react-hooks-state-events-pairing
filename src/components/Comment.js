df582a8122cf8b4afa5fe6ca73f028f293ef9543
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-06-16 15:34:16 -0400

    Add up/down vote for each comment

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -2,11 +2,18 @@ import React, {useState} from "react";
 
 function Comment({commentDetails}) {
   const {id, user, comment} = commentDetails
+  const [commentUpVotes, setCommentUpVotes] = useState(0)
+  const [commentDownVotes, setCommentDownVotes] = useState(0)
+
+  const handleCommentUpVotes = () => setCommentUpVotes(commentUpVotes + 1)
+  const handleCommentDownVotes = () => setCommentDownVotes(commentDownVotes + 1)
 
   return (
     <div className="comment">
       <h3>{user}</h3>
       <p>{comment}</p>
+      <button onClick={handleCommentUpVotes}>{commentUpVotes} 👍 </button>
+      <button onClick={handleCommentDownVotes}>{commentDownVotes} 👎 </button>
     </div>
   );
 }
