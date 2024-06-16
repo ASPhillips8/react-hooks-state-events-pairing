25fd2a8b13df83160e5658e8eab940e1af8d61da
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-06-16 15:28:27 -0400

    Hide Components with onClick

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import VideoDetails from "./VideoDetails.js";
 import video from "../data/video.js";
 import CommentList from "./CommentList.jsx";
 
 function App() {
+  const [hideComments, setHideComments] = useState(true)
+
+  console.log("app:", hideComments)
+
+  function handleHideComments() {
+    setHideComments(!hideComments)
+  }
+
+
 
   return (
     <div className="App">
       <VideoDetails videoDetails={video}/>
       <br></br>
-      <button>Hide Comments</button>
+      <button onClick={handleHideComments}>{hideComments ? "Hide Comments" : "Show Comments"}</button>
       <hr />
-      <CommentList comments={video.comments}/>
+      {hideComments && <CommentList comments={video.comments}/>}
     </div>
   );
 }
--- src/components/Comment.js
+++ src/components/Comment.js
@@ -3,9 +3,6 @@ import React, {useState} from "react";
 function Comment({commentDetails}) {
   const {id, user, comment} = commentDetails
 
-
-  console.log(commentDetails, "in comment")
-
   return (
     <div className="comment">
       <h3>{user}</h3>
